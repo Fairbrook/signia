4f8ce58e8b1f8eea9d001d3e06962e6b5f8f13ac
jmartinez <jjmartinezg.04@gmail.com>
2021-11-26 21:54:10 -0600

    Google Play Games Log In

--- Assets/Scripts/LoginController.cs
+++ Assets/Scripts/LoginController.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoginController : MonoBehaviour
 {
+    PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        .RequestServerAuthCode(false /* Don't force refresh */)
+        .Build();
+
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                var app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            } else {
+                UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
         //Screen.orientation = ScreenOrientation.Portrait;
     }
 
@@ -19,7 +44,40 @@ public class LoginController : MonoBehaviour
 
     public void onLoginIntent()
     {
-        //Screen.orientation = ScreenOrientation.Landscape;
-        SceneManager.LoadScene(1);
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        Debug.Log("Instancia inicializada exitosamente");
+
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success)
+            {
+                var authCode = PlayGamesPlatform.Instance.GetServerAuthCode();
+                Debug.Log("Datos de authCode: "+authCode +"++--");
+                Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+                Firebase.Auth.Credential credential =
+                    Firebase.Auth.PlayGamesAuthProvider.GetCredential(authCode);
+                auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+                    if (task.IsCanceled) {
+                        Debug.LogError("SignInWithCredentialAsync was canceled.");
+                        return;
+                    }
+                    if (task.IsFaulted) {
+                        Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                        return;
+                    }
+
+                    Firebase.Auth.FirebaseUser newUser = task.Result;
+                    Debug.LogFormat("User signed in successfully: {0} ({1})",
+                        newUser.DisplayName, newUser.UserId);
+                });
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                Debug.LogError("Fallo inicio de sesion");
+                SceneManager.LoadScene(0);
+            }
+        });
     }
 }
